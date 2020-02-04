4921dc27ebe78bfe6af7208aaa1fdfdad82963f5
Аlex Kukhtin <alex.kukhtin@outlook.com>
2020-02-04 21:09:47 +0200

    sql server ?

---
+++ A2v10.ProcS.SqlServer/SqlServerInstanceStorage.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Threading.Tasks;
+using A2v10.ProcS.Interfaces;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace A2v10.ProcS.SqlServer
+{
+	public class SqlServerInstanceStorage : IInstanceStorage
+	{
+		public IInstance Create(Guid processId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public async Task<IInstance> Load(Guid instanceId)
+		{
+			String cnnString = "";
+			using (var cnn = new SqlConnection(cnnString))
+			{
+				await cnn.OpenAsync();
+				using (var cmd = cnn.CreateCommand())
+				{
+					using (var rdr = await cmd.ExecuteReaderAsync(CommandBehavior.SingleRow))
+					{
+						if (await rdr.ReadAsync()) {
+
+						}
+					}
+				}
+			}
+			throw new NotImplementedException("use InstanceFactory here");
+		}
+
+		public Task Save(IInstance instance)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
--- A2v10.ProcS/Repository.cs
+++ A2v10.ProcS/Repository.cs
@@ -1,4 +1,5 @@
-﻿
+﻿// Copyright © 2020 Alex Kukhtin. All rights reserved.
+
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -13,15 +14,21 @@ namespace A2v10.ProcS
 		private readonly IWorkflowStorage _workflowStorage;
 		private readonly IInstanceStorage _instanceStorage;
 
+		private Dictionary<Guid, IInstance> _instanceCache = new Dictionary<Guid, IInstance>();
+
 		public Repository(IWorkflowStorage workflowStorage, IInstanceStorage instanceStorage)
 		{
 			_workflowStorage = workflowStorage ?? throw new ArgumentNullException(nameof(workflowStorage));
 			_instanceStorage = instanceStorage ?? throw new ArgumentNullException(nameof(instanceStorage));
 		}
 
-		public Task<IInstance> Get(Guid id)
+		async public Task<IInstance> Get(Guid id)
 		{
-			return _instanceStorage.Load(id);
+			if (_instanceCache.TryGetValue(id, out IInstance instance))
+				return instance;
+			instance = await _instanceStorage.Load(id);
+			_instanceCache.Add(id, instance);
+			return instance;
 		}
 
 		public Task Save(IInstance instance)
