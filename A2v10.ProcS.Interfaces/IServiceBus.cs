2429392023b249e2b42c5dd3e8bc6d208d19c4b0
artur.moshkola <artur.moshkola@aska.ua>
2020-02-05 16:22:33 +0200

    unlink ChatBot

--- A2v10.ProcS.Interfaces/IServiceBus.cs
+++ A2v10.ProcS.Interfaces/IServiceBus.cs
@@ -10,10 +10,54 @@ namespace A2v10.ProcS.Interfaces
 		
 	}
 
+	public class CorrelationId<T> : ICorrelationId, IEquatable<CorrelationId<T>> where T : IEquatable<T>
+	{
+		public T Value { get; set; }
+
+		/*public CorrelationId()
+		{
+			Value = default;
+		}*/
+
+		public CorrelationId(T value)
+		{
+			Value = value;
+		}
+
+		public Boolean Equals(ICorrelationId other)
+		{
+			if (other is CorrelationId<T> tt)
+				return Equals(tt);
+			return false;
+		}
+
+		public override Int32 GetHashCode()
+		{
+			return Value?.GetHashCode() ?? 0;
+		}
+
+		public Boolean Equals(CorrelationId<T> other)
+		{
+			if (Value == null) return other.Value == null;
+			return Value.Equals(other.Value);
+		}
+	}
 
 	public interface IMessage
 	{
 		ICorrelationId CorrelationId { get; }
+	}
+
+	public class MessageBase<CorrelationT> : IMessage where CorrelationT : IEquatable<CorrelationT>
+	{
+		public MessageBase(CorrelationT correlationId)
+		{
+			CorrelationId = new CorrelationId<CorrelationT>(correlationId);
+		}
+
+		public CorrelationId<CorrelationT> CorrelationId { get; }
+
+		ICorrelationId IMessage.CorrelationId => CorrelationId;
 	}
 
 	public interface ISaga
@@ -23,6 +67,15 @@ namespace A2v10.ProcS.Interfaces
 		Task Handle(IHandleContext context, IMessage message);
 	}
 
+	public abstract class SagaBase<CorrelationT> : ISaga where CorrelationT : IEquatable<CorrelationT>
+	{
+		public CorrelationId<CorrelationT> CorrelationId { get; } = new CorrelationId<CorrelationT>(default);
+
+		ICorrelationId ISaga.CorrelationId => CorrelationId;
+
+		public Boolean IsComplete { get; set; }
+		public abstract Task Handle(IHandleContext context, IMessage message);
+	}
 
 	public interface IServiceBus
 	{
